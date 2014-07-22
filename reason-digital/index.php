d5c6709086c11822fee8ce934893e836e17bf9b5
JStormrage <jad.stormrage@gmail.com>
2014-07-22 18:50:00 +0300

    Create index.php

---
+++ reason-digital/index.php
@@ -0,0 +1,87 @@
+<style type="text/css">
+@import url(http://fonts.googleapis.com/css?family=PT+Sans+Narrow);
+.card-container { 
+	display:block;
+	margin:0 auto;
+	padding: 15px;
+	margin-bottom: 30px;
+	-webkit-box-shadow: 1px 1px 2px 1px #d0d0d0;
+	-moz-box-shadow: 1px 1px 2px 1px #d0d0d0;
+	box-shadow: 1px 1px 2px 1px #d0d0d0;
+	font-family: 'PT Sans Narrow', sans-serif;
+	font-size:14px;
+	background: rgb(247, 247, 247);
+	border-radius: 3px;
+	-moz-border-radius: 3px;
+	webkit-border-radius: 3px;
+	margin-top:15px;
+	margin-left:15px;
+	float:left; 
+	min-width: 235px;
+	min-height: 1px;
+	width:90%;
+}
+
+.video-container { position:relative; overflow:hidden; height:170px; border-radius:10px;  }
+
+.card-container  h1 { color: #f67100; text-transform:uppercase; }
+
+@media (min-width: 992px) {
+	.card-container { 	
+				width:17%;
+	}
+}
+
+
+</style>
+
+
+
+
+<div class="card-container">
+	<h1> some title </h1>
+	<div class="video-container">
+		<iframe style="width:100%;"  src="https://www.youtube.com/embed/OzablsgIQ20" frameborder="0" allowfullscreen></iframe>
+	</div>
+	<p> Random text for test only </p>
+</div>
+
+<div class="card-container">
+	<h1> some title </h1>
+	<div class="video-container">
+		<iframe style="width:100%;"  src="https://www.youtube.com/embed/OzablsgIQ20" frameborder="0" allowfullscreen></iframe>
+	</div>
+	<p> Random text for test only </p>
+</div>
+
+<div class="card-container">
+	<h1> some title </h1>
+	<div class="video-container">
+		<iframe style="width:100%;"  src="https://www.youtube.com/embed/OzablsgIQ20" frameborder="0" allowfullscreen></iframe>
+	</div>
+	<p> Random text for test only </p>
+</div>
+
+<div class="card-container">
+	<h1> some title </h1>
+	<div class="video-container">
+		<iframe style="width:100%;"  src="https://www.youtube.com/embed/OzablsgIQ20" frameborder="0" allowfullscreen></iframe>
+	</div>
+	<p> Random text for test only </p>
+</div>
+
+<div class="card-container">
+	<h1> some title </h1>
+	<div class="video-container">
+		<iframe style="width:100%;"  src="https://www.youtube.com/embed/OzablsgIQ20" frameborder="0" allowfullscreen></iframe>
+	</div>
+	<p> Random text for test only </p>
+</div>
+
+<div class="card-container">
+	<h1> some title </h1>
+	<div class="video-container">
+		<iframe style="width:100%;"  src="https://www.youtube.com/embed/OzablsgIQ20" frameborder="0" allowfullscreen></iframe>
+	</div>
+	<p> Random text for test only </p>
+</div>
